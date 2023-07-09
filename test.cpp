a2c6b4f6b62880fafe89b22551304c392abdf7c5
Shamim <shamim@outlook.jp>
2023-07-09 13:07:10 +0600

    first commit

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int  sum, sub, multi;
+float div, a, b;
+
+
+cout<<"Enter the value of a ";
+cin>>a;
+cout<<"Enter the value of b ";
+cin>>b;
+cout<<"\nThe value of a "<<a;
+cout<<"\nThe value of b "<<b;
+sum = a+b;
+sub = a-b;
+multi = a*b;
+div = a/b;
+
+cout << "\n\nThe summation is "<< sum ;
+cout << "\nThe subtraction is "<< sub;
+cout << "\nThe multiplication is "<< multi;
+cout << "\nThe division is "<< div;
+}
